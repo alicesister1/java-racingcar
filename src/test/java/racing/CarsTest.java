205187e470f1ba3aabce9194a2934f7335d840ed
양경희 <alicesister1@gmail.com>
2022-04-26 22:45:34 +0900

    refactor: @BeforeEach 를 이용한 테스트 픽스처 개선

--- src/test/java/racing/CarsTest.java
+++ src/test/java/racing/CarsTest.java
@@ -1,5 +1,6 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racing.domain.Car;
@@ -14,42 +15,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
-    Cars createCars(int count) {
-        List<String> names = IntStream.range(0, count)
+    private Cars testCars = null;
+
+    @BeforeEach
+    void setUp() {
+        List<String> names = IntStream.range(0, 3)
                 .mapToObj(i -> String.format("car%d", i))
                 .collect(Collectors.toList());
-        return new Cars(names);
+        testCars = new Cars(names);
     }
 
     @DisplayName("자동차들은 이동후 Position 증가한다")
     @Test
     void carMoveForward() {
-        Cars cars = createCars(3);
-        cars.run(() -> true);
-        assertThat(cars.getCars()).allMatch(car -> car.getPosition().getCurrentPosition() == 1);
+        testCars.run(() -> true);
+        assertThat(testCars.getCars()).allMatch(car -> car.getPosition().getCurrentPosition() == 1);
     }
 
-    @DisplayName("특정 위치에 해당하는 자동차를 반환하는지 테스트")
+    @DisplayName("특정 위치에 해당하는 자동차를 반환한다")
     @Test
     void getCarsEqualsPositionTest() {
-        Cars cars = createCars(1);
-        cars.run(() -> true);
-        cars.getCars().forEach(car -> System.out.println("car.getPosition().getCurrentPosition() = " + car.getPosition().getCurrentPosition()));
-        assertThat(cars.getCarsEqualsPosition(new Position(1))).hasSize(1);
+        testCars.run(() -> true);
+        assertThat(testCars.getCarsEqualsPosition(new Position(1))).hasSize(3);
     }
 
-    @DisplayName("제일 많이 전진한 자동차의 위치 가져오기 테스트")
+    @DisplayName("제일 많이 전진한 자동차의 위치 가져온다")
     @Test
     void getMaxPositionTest() {
-        Cars cars = createCars(5);
-        run(cars.getCars(), 2);
-        assertThat(cars.getMaxPosition().getCurrentPosition()).isEqualTo(2);
-    }
-
-    private void run(List<Car> cars, int count) {
+        List<Car> cars = testCars.getCars();
         Car car = cars.get(cars.size() - 1);
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < 2; i++) {
             car.run(() -> true);
         }
+
+        assertThat(testCars.getMaxPosition().getCurrentPosition()).isEqualTo(2);
     }
 }
